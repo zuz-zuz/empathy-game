c27c4fdfeb6f386f04cc0929ad637b0b75e17dfe
Zuz <pacutovazuzana@gmail.com>
2021-01-03 09:21:59 +0100

    working prototype

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import React, { useState } from 'react';
+import inputArr from './input';
 
-function App() {
+const style = {
+  backgroundColor: 'white', 
+  color: 'black', 
+  padding: '3% 0',
+  fontSize: '200%',
+  fontWeight: 'bold',
+  borderRadius: '8px',
+  border: 'solid 2px black',
+  fontFamily: 'Raleway',
+  width: '35vw',
+  marginTop: '50px'
+}
+
+const styleDiv = {
+  display: 'flex',
+  justifyContent: 'center',
+  marginTop: '50px'
+}
+
+const RandomQuestion = (props) => {
+  if(props.usedNumbers.includes(props.randomNumber)) {
+    <div>
+      TRY AGAIN!
+    </div>
+  } else {
+    return (
+      <div>
+        {inputArr[props.randomNumber]}
+    </div>
+    )
+  }
+}
+
+
+const App = (props) => {
+  let randomNumber; 
+  const usedNumbers = [];
+
+  const randomNumberGenerator = () => {
+    randomNumber = Math.floor(Math.random() * 100);
+    return randomNumber;
+  }
+
+  const [randomQuestion, setRandomQuestion] = useState(randomNumber);
+  const handleClick = () => {
+    randomNumberGenerator();
+    usedNumbers.push(randomNumber);
+    setRandomQuestion(randomNumber);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={styleDiv}>
+        <Button variant='outlined' disableElevation style={style} onClick={handleClick}>
+          Random question
+        </Button>
+      </div>
+      <div style={styleDiv}>
+        <RandomQuestion randomNumber={randomQuestion} usedNumbers={usedNumbers} />      
+      </div>
+      <div style={styleDiv}>
+        You already used all these numbers: {usedNumbers}!
+      </div>
     </div>
   );
 }
